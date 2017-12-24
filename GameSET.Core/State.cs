5d37915d15e1687136d562d90de41a89b472329c
VariantXYZ <variantxyz@gmail.com>
2017-12-23 16:45:19 -0800

    WIP: Add projects

---
+++ GameSET.Core/State.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameSET.Core
+{
+    /// <summary>
+    /// A particular GameState
+    /// </summary>
+    public class State
+    {
+        private static State CurrentState; // Maintain a reference to the current state so we can have C exports modify it
+
+        public static void SetCurrentState(State st) => CurrentState = st;
+
+        State(bool setCurrentState = true)
+        {
+            if (setCurrentState)
+            {
+                SetCurrentState(this);
+            }
+        }
+
+        ~State()
+        {
+        }
+    }
+}
